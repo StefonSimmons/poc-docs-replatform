2d365c8be2be2c2d942f65c7e2b89159d796c2a9
StefonSimmons <s.j.simmons@spartans.nsu.edu>
2025-06-18 12:30:34 -0400

    add marked, greymatter and google sdk for parsing and reqs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          // Autogenerate a group of links for the 'guides' directory.
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Integrations",
+          link: "/integrations",
+        },
+        {
+          label: "Reference",
+          // Autogenerate a group of links for the 'reference' directory.
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
