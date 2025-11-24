b5b933e230b65c8f3fce1e62e540ce2764cc5413
StefonSimmons <s.j.simmons@spartans.nsu.edu>
2025-11-24 16:33:43 -0500

    configure astro /starlight to use markdoc

---
+++ markdoc.config.mjs
@@ -0,0 +1,30 @@
+// ./markdoc.config.json is used to configure the Markdoc extension on vscode and cursor
+
+// This file configures Astro to use Markdoc. It uses an experimental integration (@astrojs/markdoc)
+// However, in order to use starlight components as Markdoc `tags` (or rather use markdoc in starlight?), I need to extend this configuration to use the starlight-markdoc preset/config (@astrojs/starlight-markdoc).
+// This not the case for .mdx. Why are focused on .mdoc when .mdx is less messy to configure and less experimental? Is it becasue hugo will not render mdoc and we are using mdoc for C-docs?
+// see https://starlight.astro.build/guides/authoring-content/#add-markdoc-to-an-existing-project
+
+import { defineMarkdocConfig, component } from '@astrojs/markdoc/config';
+import starlightMarkdoc from '@astrojs/starlight-markdoc';
+
+
+export default defineMarkdocConfig({
+    tags: {
+        card: {
+            render: component('@astrojs/starlight/components', 'Card'),
+            attributes: {
+                title: { type: String },
+                icon: { type: String },
+            },
+        },
+        cardgrid: {
+            render: component('@astrojs/starlight/components', 'CardGrid'),
+            attributes: {
+                stagger: { type: Boolean },
+            },
+        },
+
+    },
+    extends: [starlightMarkdoc()]
+});
