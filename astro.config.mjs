f9fc7e67b5efaa2e370fb792d67a3c7e626d5b54
StefonSimmons <s.j.simmons@spartans.nsu.edu>
2026-03-30 17:14:52 -0400

    stop prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
     liveContentCollections: true,
   },
   output: 'static',
+  prefetch: false,
   adapter: vercel({
     imageService: true,
     excludeFiles: ['./src/pages/**/*.astro']
